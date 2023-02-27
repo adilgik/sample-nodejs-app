8b0d5a1cefdfb3c61c8bf5572a16ce3727d893ea
adilgik <111339373+adilgik@users.noreply.github.com>
2023-02-27 15:22:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = '0.0.0.0';
-const port = 8080;
+const port = 443;
 
 const server = http.createServer(function (request, response) {   
     response.writeHead(200, {'Content-Type': 'text/plain'});
